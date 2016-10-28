1efd78d7d92f5c7367c42a053ae1dc1bd5f1a43a
Barry Howard <BarryLeeHoward@gmail.com>
2016-10-28 13:52:07 -0400

    boxes up

--- src/js/otherAPI.js
+++ src/js/otherAPI.js
@@ -61,20 +61,63 @@ function populateMenu(results){
 				</div>
 			`
 			$(`#${curKey}`).append(menuHtml);
-			var iconsHtml = `
-				<div class="menu-icon-container">
-					<i class="fa fa-exclamation" aria-hidden="true"></i>
-					<i class="fa fa-star-o" aria-hidden="true"></i>
-					<i class="fa fa-fire-extinguisher" aria-hidden="true"></i>
-					<i class="fa fa-vine" aria-hidden="true"></i>
-				</div>
-				`
-			console.log(`#${curKey}${i}`);
-			console.log(iconsHtml)
+			var iconsHtml = `<div class="menu-icon-container">` +
+				makeIconInfo("fa-exclamation", "allergies", results[curKey][i].allergies) +
+				makeIconInfo("fa-star-o", "favorite", results[curKey][i].favorite) +
+				makeIconInfo("fa-fire-extinguisher", "spicy", results[curKey][i].spicy) +
+				makeIconInfo("fa-vine", "vegan", results[curKey][i].vegan) + "<div";
 			$(`#${curKey}${i}`).append(iconsHtml);
 		}
 	}
 };
+
+function makeIconInfo(iconName, foodParam, value){
+	if (foodParam === "allergies"){
+
+		var titleHTML = "<span>Allergy Info</span>";
+		if (value===1){
+			var descriptionHTML = "<p>This food has killed people</p>";
+		} else {
+			var descriptionHTML = "<p>Safe for consumption by landlubbers</p>"
+		}
+		var innerHTML = titleHTML + descriptionHTML;
+	} else if (foodParam === "favorite"){
+
+		var titleHTML = "<span>Favorite Info</span>";
+		if (value===1){
+			var descriptionHTML = "<p>Tales have been spread of this dish's deliciousness.</p>";
+		} else {
+			var descriptionHTML = "<p>Nobody likes this dish, but who says you aren't special?</p>"
+		}
+		var innerHTML = titleHTML + descriptionHTML;
+
+	} else if (foodParam === "spicy"){
+
+		var titleHTML = "<span>Spicy Info</span>";
+		if (value===1){
+			var descriptionHTML = "<p>Might want to wash this down with seawater</p>";
+		} else {
+			var descriptionHTML = "<p>Mild food for mild men</p>"
+		}
+		var innerHTML = titleHTML + descriptionHTML;
+
+	} else if (foodParam === "vegan"){
+		var titleHTML = "<span>Vegan Info</span>";
+		if (value===1){
+			var descriptionHTML = "<p>Mostly composed of Kelp</p>";
+		} else {
+			var descriptionHTML = "<p>Contains meat, milk, egg, or food of some sort</p>"
+		}
+		var innerHTML = titleHTML + descriptionHTML;
+	}
+	var iconHTML = `
+	<i class="fa ${iconName}" aria-hidden="true">
+	<div class="icon-info-box">
+		${innerHTML}
+	</div></i>
+	`;
+	return iconHTML;
+}
 
 // all this fancy code that Barry taught me :D
 
